9eb6ff5ddc280773cadc6906ad5362262c6fcb11
Christopher McElvogue <chris.mcelvogue@gmail.com>
2021-12-16 17:23:43 -0800

    commit tree 76bfb7f3981994a990b045ac43a53cd2745ecad5

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+  if (pickup >= 42) {
+    return pickup - 42;
+  } else {
+    return 42 - pickup;
+  }
+}
+
+function distanceFromHqInFeet(start) {
+  return distanceFromHqInBlocks(start) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  if (start < destination) {
+    return (destination - start) * 264;
+  } else {
+    return (start - destination) * 264;
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  let total = distanceTravelledInFeet(start, destination);
+  if (total <= 400) {
+    return 0;
+  } else if (total > 400 && total <= 2000) {
+    return (total - 400) * 0.02;
+  } else if (total >= 2000 && total <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
